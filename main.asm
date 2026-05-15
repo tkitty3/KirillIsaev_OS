f54a063b0f1cd8b371ad15cfcb7d9d3045c3f248
tkitty3 <nashasemya7ya@gmail.com>
2026-05-15 16:45:51 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,18 +2,38 @@ org 0x7C00
 bits 16
 start:
 cli
-mov ax, 0
+mov ax, 0 ; системная хрень для зануления
 mov ds, ax
 mov es, ax
 mov ss, ax
-mov sp, 0x7C00
+mov sp, 0x7C00 ; зовем биос из 1 строчки (кто вообще будет этр читать)
 sti
-mov si, message
-print:
+
+mov ah, 0x06 ;очистка экрана от надписей биоса шоб был только кирюша
+mov al, 0
+mov bh, 0xF0
+mov ch, 0
+mov cl, 0
+mov dh, 24
+mov dl, 79
+int 0x10
+
+mov ah, 0x02 ; зовем системных переключить курсор а то вдруг ну на всякий пожарный
+mov bh, 0
+mov dh, 0
+mov dl, 0
+int 0x10
+
+
+mov si, message ; печать наконецто
+
+
+print: ;ф-ия печати 
 lodsb
 cmp al, 0
 je halt
 mov ah, 0x0E
+mov bl, 0xF0
 int 0x10
 jmp print
 
@@ -25,9 +45,6 @@ cli
 hlt 
 jmp .loop
 
-
-
-
-message: db "KIRILLISAEV_OS", 0
-times 510 - ($ - $$) db 0 
+message: db "KIRILLISAEV_OS", 0 ;когда кирилл и исаев
+times 510 - ($ - $$) db 0  ;зануление остатка на 510бит
 dw 0xAA55
