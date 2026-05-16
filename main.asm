1ea76363fc5c6cfc77abaa70f1f09da2017838ba
tkitty3 <nashasemya7ya@gmail.com>
2026-05-16 15:08:49 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -18,12 +18,27 @@ resb 16384
 stack_t:
 section .text
 global start
+global outb
 extern kmain
 start:
 mov esp, stack_t
 call kmain
 cli
 
+
+
 .halt:
 hlt
 jmp .halt
+
+global inp
+inp:
+mov edx, [esp + 4]
+in al, dx
+ret
+
+outb:             
+    mov edx, [esp + 4] 
+    mov al, [esp + 8]  
+    out dx, al
+    ret
