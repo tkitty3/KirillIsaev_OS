b3e4665c7db5f2ce724dc3761528063ec0c1273e
tkitty3 <nashasemya7ya@gmail.com>
2026-05-16 14:38:46 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
-nasm -f bin boot.asm -o os.img && qemu-system-i386 -drive format=raw,file=os.img
+#!/bin/bash
+set -e
+
+#компиляция
+nasm -f elf32 boot.asm -o loader.o
+gcc -m32 -ffreestanding -c kernel.c -o kernel.o
+ld -m elf_i386 --no-warn-rwx-segments -T link.ld -o kernel.bin loader.o kernel.o
+
+#делаем груб
+mkdir -p isodir/boot/grub
+cat << 'EOF' > isodir/boot/grub/grub.cfg
+menuentry "KirillIsaevOS" {
+    multiboot /boot/kernel
+}
+EOF
+
+#исо файл
+cp kernel.bin isodir/boot/kernel
+grub-mkrescue -d /usr/lib/grub/i386-pc/ -o kirillisaevos.iso isodir
