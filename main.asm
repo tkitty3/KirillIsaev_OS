96a0ca111b4092d13c1c960431069fa198de1a86
tkitty3 <nashasemya7ya@gmail.com>
2026-05-16 14:38:21 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,51 +1,29 @@
-org 0x7C00           
-bits 16              
+bits 32
 
+MAGIC       equ 0x1BADB002
+FLAGS       equ 0x0
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
+
+section .bss
+align 16
+stack_b:
+resb 16384
+
+stack_t:
+section .text
+global start
+extern kmain
 start:
-    xor ax, ax
-    mov ds, ax
-    mov es, ax  
-   
-    int 0x10        
-mov si, hello_os 
-print:
-lodsb
-cmp al, 0
-je printl
-mov ah, 0x0e
-int 0x10
-jmp print
+mov esp, stack_t
+call kmain
+cli
 
-mov si, helping_l
-printl:
-lodsb
-cmp al, 0
-je key
-mov ah, 0x0e
-int 0x10
-jmp print
-
-mov si, help
-key:
-    mov ah, 0x00
-    int 0x16          
-    cmp al, 'h'        
-    jne key        
-mov si, help      
-print_help:
-    lodsb              
-    cmp al, 0         
-    je key             
-    mov ah, 0x0e       
-    int 0x10           
-    jmp print_help  
-    \
-halt:
-    jmp halt       
-
-help db "https://github.com/tkitty3/KirillIsaev_OS.git", 13, 10, 13, 10, 0
-hello_os db "KIRILLISAEV_OS", 13, 10, 13, 10, 0
-helping_l db "press h", 13, 10, 0
-times 510 - ($ - $$) db 0  
-dw 0xAA55            
-
+.halt:
+hlt
+jmp .halt
